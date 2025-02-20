728eba8ae0e7abfa7b8b7fd7495dcc0e82653e99
Rodgers Chayuga <chayugarodgers@gmail.com>
2025-02-20 13:10:43 +0300

    smart-contract | setup for Lisk Deployment

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -3,11 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-ethers";
 
+require('dotenv').config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   typechain: {
     outDir: "typechain-types",
     target: "ethers-v6",
+  },
+  networks: {
+    // for testnet
+    'lisk-sepolia': {
+      url: 'https://rpc.sepolia-api.lisk.com',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
   },
 };
 
